d6c997c16b9deb31f767ac2c48752dbc87a82199
david0305 <kmchman@gmail.com>
2021-01-26 11:52:41 +0900

    UI테스트 씬 추가

---
+++ UITest/Assets/Script/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void OnClickTestButton()
+    {
+        PopupWindow.instance.PopupCheckWindowOpen(() => {
+            Debug.Log("tetstest");
+        }, "test", PopupWindow.MsgType.notice, "msg");
+    }
+}
