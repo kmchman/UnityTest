d7d3390a435d3f22d4bd091bde3ed72ea5d19f12
david0305 <kmchman@gmail.com>
2021-04-13 12:40:17 +0900

    default

---
+++ MiniGame/Minigame/Assets/Script/MoleGame/MoleGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleGameManager : MonoBehaviour
+{
+    [SerializeField] private MoleObject[] moleObjects;
+
+    private Coroutine gameCoroutine;
+    private void Start()
+    {
+        foreach (var item in moleObjects)
+            item.gameObject.SetActive(false);
+
+        if (gameCoroutine != null)
+            StopCoroutine(gameCoroutine);
+        StartCoroutine(GameCoroutine());
+    }
+
+    IEnumerator GameCoroutine()
+    {
+        while (true)
+        {
+            int rand = Random.Range(0, moleObjects.Length);
+            moleObjects[rand].AppearMole();
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ MiniGame/Minigame/Assets/Script/MoleGame/MoleObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoleObject : MonoBehaviour
+{
+    [SerializeField] private Sprite moleSpr;
+
+    private Coroutine appearCoroutine;
+    public void AppearMole()
+    {
+        gameObject.SetActive(true);
+        if (appearCoroutine != null)
+            StopCoroutine(appearCoroutine);
+        appearCoroutine = StartCoroutine(AppearCoroutine());
+    }
+
+    IEnumerator AppearCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
