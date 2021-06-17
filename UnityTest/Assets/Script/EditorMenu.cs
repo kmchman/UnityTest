689a73ced720f08f82e8bbf8e0a30ff768793225
david0305 <kmchman@gmail.com>
2021-06-17 18:12:56 +0900

    AddressableAsset Test

---
+++ UnityTest/Assets/Script/EditorMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorMenu
+{
+
+    [MenuItem("Util/CleanCache")]
+    public static void CleanCache()
+    {
+        if (Caching.ClearCache())
+        {
+            EditorUtility.DisplayDialog("알림", "캐시가 삭제되었습니다.", "확인");
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("오류", "캐시 삭제에 실패했습니다.", "확인");
+        }
+    }
+}
